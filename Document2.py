6f65fe4e42c09419d9d1bc842737075be6d6cfdb
himopantsu <57516419+himopantsu@users.noreply.github.com>
2021-06-13 06:05:05 +0900

    Update Document2.py

--- Document2.py
+++ Document2.py
@@ -77,7 +77,7 @@ async def on_message(message):
 		await dm.send(f"{message.author.mention}さんにダイレクトメッセージ")
 		
 	elif message.content == "!ダンス寿司":
-		vc = await message.author.voice.channel.connect()
+		vc = message.author.voice.channel.connect()
 		vc.play(discord.FFmpegPCMAudio('dancesushi.mp3'))
 
 client.run(bot_token)
